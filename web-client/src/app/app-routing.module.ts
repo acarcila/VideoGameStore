025f66359fc9b0dd571467a0ae8265c8bf6c7b5b
Andrés Camilo Arcila <acarcila98@gmail.com>
2020-09-18 17:08:04 -0500

    Implemented a basic routing

--- web-client/src/app/app-routing.module.ts
+++ web-client/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AdminViewComponent} from './admin-view/admin-view.component';
+import {ClientViewComponent} from './client-view/client-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'admin', component: AdminViewComponent},
+  {path: 'client', component: ClientViewComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
